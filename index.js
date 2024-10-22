65795654ebb1a6ab647d6fde182abe3eb32264af
Ksune <felixmorningstar@proton.me>
2024-10-22 08:35:09 +0200

    Uploaded 3D project
    Inital commit for my version of cartomantica its in 3D and its in very very early stage

---
+++ index.js
@@ -0,0 +1,34 @@
+// main.js
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false, // Needed for integrating three.js without security concerns
+      preload: path.join(__dirname, 'preload.js')
+    }
+  });
+
+  win.loadFile('./web/src/index.html');
+  win.maximize();
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
