df7021045235b8bdfa0fe07a0f18a3d72ed88095
Valenta Hanus <hanus.valenta@gmail.com>
2024-11-06 19:06:50 +0100

    hide the bar

--- index.js
+++ index.js
@@ -1,35 +1,38 @@
-// main.js
+// index.js
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
 
 function createWindow() {
-  const win = new BrowserWindow({
-    width: 800,
-    height: 600,
-    icon: path.join(__dirname, 'icon.png'),
-    webPreferences: {
-      nodeIntegration: true,
-      contextIsolation: false, // Needed for integrating three.js without security concerns
-      preload: path.join(__dirname, 'preload.js')
-    }
-  });
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        icon: path.join(__dirname, 'icon.png'),
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false, // Needed for integrating three.js without security concerns
+            preload: path.join(__dirname, 'preload.js')
+        }
+    });
 
-  win.loadFile('./web/src/index.html');
-  win.maximize();
+    win.loadFile('./web/src/index.html');
+    win.maximize();
+
+    // Hide the menu bar
+    win.setMenuBarVisibility(false);
 }
 
 app.whenReady().then(() => {
-  createWindow();
+    createWindow();
 
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow();
-    }
-  });
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow();
+        }
+    });
 });
 
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
 });
